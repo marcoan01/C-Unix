83bc831ce272de8544d57e28b8da953d6581720b
Marco Delgado <57053359+marcoan01@users.noreply.github.com>
2023-03-28 12:48:36 -0500

    Update file2.cc

--- shell_script/file2.cc
+++ shell_script/file2.cc
@@ -2,8 +2,6 @@
  *Filename: file2.cc
  *Date: 02/07/2021
  *Author Marco Delgado
- *Email mad190003@utdallas.edu
- *Course CS 3377.0W6 Spring 2021
  *Version 1.0
  *Copyright  2021, all rights reserved
  *Description: 
