a13d4fc73270099115e754769053f92011cf5a0f
Marco Delgado <57053359+marcoan01@users.noreply.github.com>
2023-03-28 12:47:51 -0500

    Update compile2.sh

--- shell_script/compile2.sh
+++ shell_script/compile2.sh
@@ -4,8 +4,6 @@
 # Filename compile2.sh
 # Date 02/07/2021
 # Author Marco Delgado
-# Email mad190003@utdallas.edu
-# Course CS 3377.OW6, spring 2021
 # Version 1.0
 # Copyright 2021, all rights reserved
 # Description: Running myprogram with different amount of arguments 
